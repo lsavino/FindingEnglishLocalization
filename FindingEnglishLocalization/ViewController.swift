5347b9537f10649f4334d4b5fbfaeb885f275fd8
Laura Savino <laura@khanacademy.org>
2015-05-06 11:15:25 -0700

    Initial Commit

---
+++ FindingEnglishLocalization/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FindingEnglishLocalization
+//
+//  Created by Laura Savino on 5/6/15.
+//  Copyright (c) 2015 Laura Savino. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
