6e174fd27facc7edd83e52606edafd0967eb8d13
Laura Savino <laura@khanacademy.org>
2015-05-06 14:21:48 -0700

    Initial commit with a couple localizations

--- FindingEnglishLocalization/ViewController.swift
+++ FindingEnglishLocalization/ViewController.swift
@@ -10,9 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+	let helloLabel = UILabel()
+	let localeLabel = UILabel()
+
+	override func loadView() {
+		super.loadView()
+
+		view.addSubview(helloLabel)
+		view.addSubview(localeLabel)
+
+		helloLabel.setTranslatesAutoresizingMaskIntoConstraints(false)
+		localeLabel.setTranslatesAutoresizingMaskIntoConstraints(false)
+
+		let viewsDict = [ "hello" : helloLabel, "locale" : localeLabel ]
+		view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:|-(100)-[hello(>=50)]-(30)-[locale(>=50)]-(>=100)-|", options: .AlignAllCenterX, metrics: nil, views: viewsDict))
+		let centeredHorizontalConstraint = NSLayoutConstraint(item: self.view, attribute: .CenterX, relatedBy: .Equal, toItem: helloLabel, attribute: .CenterX, multiplier: 1, constant: 0)
+		view.addConstraint(centeredHorizontalConstraint)
+	}
+
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
+
+		helloLabel.text = NSLocalizedString("Hello friends", comment: "A greeting to friends")
+		localeLabel.text = NSLocale.autoupdatingCurrentLocale().objectForKey(NSLocaleLanguageCode) as! String
+
 	}
 
 	override func didReceiveMemoryWarning() {
